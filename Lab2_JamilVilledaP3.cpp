540546d2fa395bf33e4e827866d4372b5f0567ad
Jamil <jamvilleda97@live.com>
2018-05-04 14:54:02 -0600

    Primer push

---
+++ Lab2_JamilVilledaP3.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+#include <string>
+#include <math.h>
+#include <ctype.h>
+using namespace std;
+
+void circulos(){
+int opcion = 0;
+	
+cout<< "Ingrese el metodo por el que desea ver la colision de los circulos"<<endl;
+cout<< "1. Ingresar coordenadas manualmente"<<endl;
+
+
+
+
+
+
+}
+
+
+void triangulos(){
+int contadorLetras = 0;
+
+char lado1triangulo1 = '0';
+char lado2triangulo1 = '0';
+char lado3triangulo1 = '0';
+char angulo1triangulo1 = '0';
+char angulo2triangulo1 = '0';
+char angulo3triangulo1 = '0';
+
+char lado1triangulo2 = '0';
+char lado2triangulo2 = '0';
+char lado3triangulo2 = '0';
+char angulo1triangulo2 = '0';
+char angulo2triangulo2 = '0';
+char angulo3triangulo2 = '0';
+
+cout<<"Ingrese el primer lado del primer triangulo"<<endl;
+cin>>lado1triangulo1;
+if(isdigit(lado1triangulo1){
+int lado1 = 
+
+}		
+cout<<"Ingrese el segundo lado del primer triangulo"<<endl;
+cin>>lado2triangulo1;
+cout<<"Ingrese el tercer lado del primer triangulo"<<endl;
+cin>>lado3triangulo1;
+cout<<"Ingrese el primer angulo del primer triangulo"<<endl;
+cin>>angulo1triangulo1;
+cout<<"Ingrese el segundo angulo del primer triangulo"<<endl;
+cin>>angulo2triangulo1;
+cout<<"Ingrese el tercer angulo del primer triangulo"<<endl;
+cin>>angulo3triangulo1;
+cout<< "Ingrese el primer lado del segundo triangulo"<<endl;
+cin>>lado1triangulo2;
+cout<<"Ingrese el segundo lado del segundo triangulo"<<endl;
+cin>>lado2triangulo2;
+cout<<"Ingrese el tercer lado del segundo triangulo"<<endl;
+cin>>lado3triangulo2;
+cout<<"Ingrese el primer angulo del segundo triangulo"<<endl;
+cin>>angulo1triangulo2;
+cout<<"Ingrese el segundo angulo del segundo triangulo"<<endl;
+cin>>angulo2triangulo2;
+cout<<"Ingrese el tercer angulo del segundo triangulo"<<endl;
+cin>>angulo3triangulo2;
+
+}
+
+
+
+int main(){
+int opcion = 0;
+cout<<"Porfavor ingrese la opcion deseada: "<<endl;
+
+cin>>opcion;
+
+if (opcion==1){
+
+triangulos();
+
+
+}
+
+
+if (opcion==2){
+
+
+}
+
+if (opcion==3){
+
+
+}
+  
+}
